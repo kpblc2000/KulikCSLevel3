44d1658fc23f5939181336382edba78369ce574d
Alexey Kulik <kpblc2000@gmail.com>
2020-10-01 19:24:46 +0300

    Повтор урока, исключение собственных ошибок и пошаговый проход

---
+++ Tests/WPFTests/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFTests.Infrastructure.Commands.Base;
+
+namespace WPFTests.Infrastructure.Commands
+{
+    class CloseWindowCommand : Command
+    {
+        protected override void Execute(object Parameter)
+        {
+            var win = Parameter as Window;
+            if (win is null)
+                win = Application.Current.Windows.Cast<Window>().FirstOrDefault(x => x.IsFocused);
+            if (win is null)
+                win = Application.Current.Windows.Cast<Window>().FirstOrDefault(x => x.IsActive);
+
+            win?.Close();
+        }
+    }
+}
---
+++ Tests/WPFTests/VIewModels/MainWindowsViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using WPFTests.Infrastructure.Commands;
+using WPFTests.VIewModels.Base;
+
+namespace WPFTests.VIewModels
+{
+    class MainWindowsViewModel : ViewModel
+    {
+        private string _title = "test window";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+            /*
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                // OnPropertyChanged(nameof(Title));
+                OnPropertyChanged();
+            }
+            */
+        }
+
+        private bool _timerEnabled = true;
+        public bool TimerEnabled
+        {
+            get => _timerEnabled;
+            set
+            {
+                if (!Set(ref _timerEnabled, value)) return;
+                _timer.Enabled = value;
+            }
+        }
+
+        private readonly Timer _timer;
+
+        public DateTime CurrentTime => DateTime.Now;
+
+        public MainWindowsViewModel()
+        {
+            _timer = new Timer(100);
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            OnPropertyChanged(nameof(CurrentTime));
+        }
+
+        // ПОпытка работы с командами
+
+        private ICommand _ShowDlgCmd;
+
+        public ICommand ShowDialogCommand
+        {
+            get
+            {
+                if (_ShowDlgCmd is null)
+                {
+                    _ShowDlgCmd = new RelayCommand(OnShowDialogCommandExecute);
+                }
+                return _ShowDlgCmd;
+            }
+        }
+
+        private void OnShowDialogCommandExecute(object obj)
+        {
+            MessageBox.Show("Hi");
+        }
+
+    }
+}
