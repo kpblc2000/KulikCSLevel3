1832048faeb3f66c9e1baac1f498ed26acef1b7a
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 23:45:33 +0300

    Ввод команд записи-чтения. Без тестирования. И без вывода "Наружу"

--- KulikCSLevel3/ViewModels/MainWindowViewModel.cs
+++ KulikCSLevel3/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,136 @@
-﻿using KulikCSLevel3.ViewModels.Base;
+﻿using KulikCSLevel3.Data;
+using KulikCSLevel3.Infrastructure.Commands;
+using KulikCSLevel3.Models;
+using KulikCSLevel3.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace KulikCSLevel3.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
+        private static string __DataFileName = "data.xml";
+
+        private TestData _savedDatas;
+
         private string _Title = "Кулик : Рассылка почты";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
+
+        #region Данные и свойства
+        // Вывод данных из представления
+        private ObservableCollection<Sender> _senders;
+        private ObservableCollection<Server> _servers;
+        private ObservableCollection<Recipient> _recipients;
+        private ObservableCollection<Message> _messages;
+
+        public ObservableCollection<Server> Servers
+        {
+            get => _servers;
+            set => Set(ref _servers, value);
+        }
+
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _recipients;
+            set => Set(ref _recipients, value);
+        }
+
+        public ObservableCollection<Sender> Senders
+        {
+            get => _senders;
+            set => Set(ref _senders, value);
+        }
+
+        public ObservableCollection<Message> Messages
+        {
+            get => _messages;
+            set => Set(ref _messages, value);
+        }
+
+        private Server _selServer;
+        public Server SelectedServer
+        {
+            get => _selServer;
+            set => Set(ref _selServer, value);
+        }
+
+        private Sender _selSender;
+        public Sender SelectedSender
+        {
+            get => _selSender;
+            set => Set(ref _selSender, value);
+        }
+
+        private Recipient _selRecipient;
+        public Recipient SelectedRecipient
+        {
+            get => _selRecipient;
+            set => Set(ref _selRecipient, value);
+        }
+
+        private Message _selMessage;
+        public Message SelectedMessage
+        {
+            get => _selMessage;
+            set => Set(ref _selMessage, value);
+        }
+        #endregion
+
+        #region Команды сериализации
+
+        private ICommand _LoadDataCommand;
+        public ICommand LoadDataCommand
+        {
+            get
+            {
+                if (_LoadDataCommand is null)
+                {
+                    _LoadDataCommand = new RelayCommand(OnLoadDataCommandExecuted);
+                }
+                return _LoadDataCommand;
+            }
+        }
+
+        private void OnLoadDataCommandExecuted(object obj)
+        {
+            _savedDatas = File.Exists(__DataFileName) ? TestData.LoadFromXML(__DataFileName) : new TestData();
+
+            Servers = new ObservableCollection<Server>(_savedDatas.Servers);
+            Senders = new ObservableCollection<Sender>(_savedDatas.Senders);
+            Recipients = new ObservableCollection<Recipient>(_savedDatas.Recipients);
+            Messages = new ObservableCollection<Message>(_savedDatas.Messages);
+        }
+
+        private ICommand _SaveDataCommand;
+        public ICommand SaveDataCommand
+        {
+            get
+            {
+                if (_SaveDataCommand is null)
+                {
+                    _SaveDataCommand = new RelayCommand(OnSaveDataCommandExecuted);
+                }
+                return _SaveDataCommand;
+            }
+        }
+
+        private void OnSaveDataCommandExecuted(object o)
+        {
+            _savedDatas.SaveToXML(__DataFileName);
+        }
+
+        #endregion
+
+
     }
 }
