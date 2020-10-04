45d0bada5a413133650b44c57e8f4ed05f0879c6
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 21:08:13 +0300

    Добавление команды закрытия окна

--- KulikCSLevel3/Infrastrucuture/Commands/Base/Command.cs
+++ KulikCSLevel3/Infrastrucuture/Commands/Base/Command.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace KulikCSLevel3.Infrastrucuture.Command.Base
+namespace KulikCSLevel3.Infrastrucuture.Commands.Base
 {
     /// <summary>
     /// Общее описание команды для окна
---
+++ KulikCSLevel3/Infrastrucuture/Commands/CloseWindowCommand.cs
@@ -0,0 +1,24 @@
+﻿using KulikCSLevel3.Infrastrucuture.Commands.Base;
+using System.Linq;
+using System.Windows;
+
+namespace KulikCSLevel3.Infrastrucuture.Commands
+{
+    class CloseWindowCommand : Command
+
+    {
+        protected override void Execute(object parameter)
+        {
+            var window = parameter as Window;
+            if (window is null)
+                window = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(w => w.IsFocused);
+            if (window is null)
+                window = Application.Current.Windows
+                .Cast<Window>()
+                .FirstOrDefault(w => w.IsActive);
+            window?.Close();
+        }
+    }
+}
