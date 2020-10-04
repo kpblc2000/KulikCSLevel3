413267f265d70fe9de6168a39a5b7b36fe2ac4b7
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 20:44:00 +0300

    Восстановление базовой модели

---
+++ KulikCSLevel3/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulikCSLevel3.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged();
+            return true;
+        }
+    }
+}
