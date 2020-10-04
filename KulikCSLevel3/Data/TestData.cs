4ea76766baa3067a9fad1e224bf2c981296bdc05
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 23:25:59 +0300

    Инициализация данных

--- KulikCSLevel3/Data/TestData.cs
+++ KulikCSLevel3/Data/TestData.cs
@@ -5,29 +5,93 @@ using System.Linq;
 
 namespace KulikCSLevel3.Data
 {
-    public static class TestData
+    public class TestData
     {
-        public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
-            .Select(i => new Sender { Name = $"Отправитель {i}", EmailAdress = $"sender{i}@server.ru" }
-            ).ToList();
 
-        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 20)
-            .Select(i => new Recipient { Name = $"Получатель {i}", EmailAdress = $"client{i}@server.ru" }
-            ).ToList();
+        private List<Sender> _Senders;
 
-        public static List<Server> Servers { get; } = Enumerable.Range(1, 5)
-            .Select(i => new Server
+        public List<Sender> Senders
+        {
+            get
             {
-                Adress = $"smtp.server{i}.com",
-                Port = 25,
-                UseSSL = (i % 2 == 0),
-                Login = $"login-{i}",
-                Passord = TextEncoder.Encoder($"pwd{i}", 1)
+                if (_Senders is null)
+                {
+                    _Senders = Enumerable.Range(1, 10).Select(i => new Sender { Name = $"Отправитель {i}", EmailAdress = $"sender{i}@server.ru" }).ToList();
+                }
+                return _Senders;
             }
-            ).ToList();
+            set { _Senders = value; }
+        }
 
-        public static List<Message> Messages { get; } = Enumerable.Range(1, 15)
+        private List<Recipient> _Recipients;
+
+        public List<Recipient> Recipients
+        {
+            get
+            {
+                if (_Recipients is null)
+                {
+                    _Recipients = Enumerable.Range(1, 10).Select(i => new Recipient { Name = $"Получатель {i}", EmailAdress = $"client{i}@server.ru" }).ToList();
+                }
+                return _Recipients;
+            }
+            set { _Recipients = value; }
+        }
+
+        private List<Server> _Servers;
+
+        public List<Server> Servers
+        {
+            get
+            {
+                if (_Servers is null)
+                    _Servers = Enumerable.Range(1, 5).Select(i => new Server
+                    {
+                        Adress = $"smtp.server{i}.com",
+                        Port = 25,
+                        Login = $"login-{i}",
+                        Passord = TextEncoder.Encoder($"pwd{i}", 1),
+                        UseSSL = (i % 2 == 0)
+                    }).ToList();
+                return _Servers;
+            }
+            set { _Servers = value; }
+        }
+
+        private List<Message> _Messages;
+
+        public List<Message> Messages
+        {
+            get
+            {
+                if (_Messages is null)
+                {
+                    _Messages = Enumerable.Range(1, 15)
             .Select(i => new Message { Subject = $"Subj{i}", Body = $"Body{i}" })
             .ToList();
+                }
+                return _Messages;
+            }
+            set { _Messages = value; }
+        }
+
+        //public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 20)
+        //    .Select(i => new Recipient { Name = $"Получатель {i}", EmailAdress = $"client{i}@server.ru" }
+        //    ).ToList();
+
+        //public static List<Server> Servers { get; } = Enumerable.Range(1, 5)
+        //    .Select(i => new Server
+        //    {
+        //        Adress = $"smtp.server{i}.com",
+        //        Port = 25,
+        //        UseSSL = (i % 2 == 0),
+        //        Login = $"login-{i}",
+        //        Passord = TextEncoder.Encoder($"pwd{i}", 1)
+        //    }
+        //    ).ToList();
+
+        //public static List<Message> Messages { get; } = Enumerable.Range(1, 15)
+        //    .Select(i => new Message { Subject = $"Subj{i}", Body = $"Body{i}" })
+        //    .ToList();
     }
 }
