a75b269da32b43b858c1359ce938775619d42b15
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 20:54:38 +0300

    Базовая команда

---
+++ KulikCSLevel3/Infrastrucuture/Command/Base/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KulikCSLevel3.Infrastrucuture.Command.Base
+{
+    /// <summary>
+    /// Общее описание команды для окна
+    /// </summary>
+    public abstract class Command : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+
+        void ICommand.Execute(object parameter) => Execute(parameter);
+
+        protected virtual bool CanExecute(object parameter) => true;
+
+        protected abstract void Execute(object parameter);
+    }
+}
