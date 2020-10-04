877b87a7fd5b4384f0733e247feb2e0de92c9a46
Alexey Kulik <kpblc2000@gmail.com>
2020-10-04 20:50:24 +0300

    Привязка к модели

---
+++ KulikCSLevel3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using KulikCSLevel3.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulikCSLevel3.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Кулик : Рассылка почты";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
