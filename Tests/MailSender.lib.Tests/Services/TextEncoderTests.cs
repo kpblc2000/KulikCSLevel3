eddf6f653e326273b103483a9ed03d3533f75107
Alexey Kulik <kpblc2000@gmail.com>
2020-10-07 23:20:57 +0300

    UnitTest

---
+++ Tests/MailSender.lib.Tests/Services/TextEncoderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfMailSender.Services;
+
+namespace MailSender.lib.Tests.Services
+{
+    /*
+     [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+     */
+
+    [TestClass]
+    public class TextEncoderTests
+    {
+
+        [TestMethod]
+        public void Encode_ABC_to_BCD_Key1()
+        {
+            const string baseString = "ABC";
+            int key = 1;
+
+            const string expectedString = "BCD";
+
+            //
+
+            string actualString = TextEncoder.Encoder(baseString, key);
+
+            //
+
+            Assert.AreEqual(expectedString, actualString);
+
+        }
+    }
+}
